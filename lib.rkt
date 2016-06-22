620335dc68502e7386f4e1bbd763f5b2e0770bc0
Victor Sebben <victorhfsebben@gmail.com>
2016-06-21 23:34:05 -0300

    Implement star functions

--- lib.rkt
+++ lib.rkt
@@ -7,6 +7,8 @@
 ;; about it: (null? lat) and else.
 ;; When recurring on a number, n, ask two questions about
 ;; it: (zero? n) and else.
+;; When recurring on a list of S-expressions, l, ask three questions
+;; about it: (null? l), (atom? (car l)), and else.
 
 ;; Second Commandment
 ;; -> Use cons to build lists.
@@ -16,9 +18,14 @@
 ;; cons it onto the natural recursion.
 
 ;; The Fourth Commandment
-;; -> Always change at least one argument while recurring. It must be
-;; changed to be closer to termination. The changing argument must be
-;; tested in the termination condition:
+;; -> Always change at least one argument while recurring.
+;; When recurring on a list of atoms, lat, use (cdr lat). When
+;; recurring on a number, n, use (sub1 n). And when recurring on
+;; a list of S-expressions, l, use (car l) and (cdr l) if neither
+;; (null? l) nor (atom? (car l)) are true.
+;;
+;; It must be changed to be closer to termination.
+;; The changing argument must be tested in the termination condition:
 ;; when using cdr, test termination with null? and
 ;; when using sub1, test termination with zero?
 
@@ -32,6 +39,9 @@
 ;;
 ;; When building a value with cons, always consider () for the value of the
 ;; terminating line.
+
+;; The Sixth Commandment
+;; Simplify only after the function is correct.
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; The proof of the pudding is in the eating. ;;
@@ -79,13 +89,13 @@
 ;;                         (rember a
 ;;                                 (cdr lat)))))))))
 
-(define rember
-  (lambda (a lat)
-    (cond
-     ((null? lat) (quote ()))
-     ((eq? (car lat) a) (cdr lat))
-     (else (cons (car lat)
-                 (rember a (cdr lat)))))))
+;; (define rember
+;;   (lambda (a lat)
+;;     (cond
+;;      ((null? lat) (quote ()))
+;;      ((eq? (car lat) a) (cdr lat))
+;;      (else (cons (car lat)
+;;                  (rember a (cdr lat)))))))
 
 ;; Contract: firsts : list  ->  list
 ;; Purpose: to return the first element of each
@@ -327,3 +337,115 @@
        (occur a (cdr lat))))
      (else
       (occur a (cdr lat))))))
+
+(define rember*
+  (lambda (a l)
+    (cond
+     ((null? l) (quote ()))
+     ((list? (car l)) (cons (rember* a (car l)) (rember* a (cdr l))))
+     ((eq? a (car l)) (rember* a (cdr l)))
+     (else (cons (car l)
+                 (rember* a (cdr l)))))))
+
+
+(define insertR*
+  (lambda (new old l)
+    (cond
+     ((null? l) (quote ()))
+     ((list? (car l))
+      (cons (insertR* new old (car l))
+            (insertR* new old (cdr l))))
+     ((eq? old (car l))
+      (cons (car l)
+            (cons new (insertR* new old (cdr l)))))
+     (else
+      (cons (car l)
+            (insertR* new old (cdr l)))))))
+
+(define occur*
+  (lambda (a l)
+    (cond
+     ((null? l) 0)
+     ((list? (car l)) (plus (occur* a (car l)) (occur* a (cdr l))))
+     ((eq? a (car l)) (add1 (occur* a (cdr l))))
+     (else (occur* a (cdr l))))))
+
+(define subst*
+  (lambda (new old l)
+    (cond
+     ((null? l) (quote ()))
+     ((list? (car l)) (cons (subst* new old (car l)) (subst* new old (cdr l))))
+     ((eq? old (car l)) (cons new (subst* new old (cdr l))))
+     (else (cons (car l) (subst* new old (cdr l)))))))
+
+(define insertL*
+  (lambda (new old l)
+    (cond
+     ((null? l) (quote ()))
+     ((list? (car l))
+      (cons (insertL* new old (car l))
+            (insertL* new old (cdr l))))
+     ((eq? old (car l))
+      (cons new
+            (cons (car l) (insertL* new old (cdr l)))))
+     (else
+      (cons (car l)
+            (insertL* new old (cdr l)))))))
+
+(define member*
+  (lambda (a l)
+    (cond
+     ((null? l) #f)
+     ((list? (car l)) (or (member* a (car l)) (member* a (cdr l))))
+     (else (or (eq? (car l) a)
+               (member* a (cdr l)))))))
+
+(define lefmost
+  (lambda (l)
+    (cond
+     ((null? l) #f)
+     ((list? (car l)) (lefmost (car l)))
+     (else (car l)))))
+
+;; Little Schemer version, with the (null?) test already added
+(define lm
+  (lambda (l)
+    (cond
+     ((null? l) #f)
+     ((atom? (car l)) (car l))
+     (else (lm (car l))))))
+
+;; (define eqlist?
+;;   (lambda (l1 l2)
+;;     (cond
+;;      ((and (null? l1) (null? l2)) #t)
+;;      ((or (null? l1) (null? l2)) #f)
+;;      ((and (list? (car l1)) (list? (car l2)))
+;;       (and (eqlist? (car l1) (car l2)) (eqlist? (cdr l1) (cdr l2))))
+;;      ((eqan? (car l1) (car l2))
+;;       (and (eqlist? (cdr l1) (cdr l2))))
+;;      (else #f))))
+
+(define equal?
+  (lambda (s1 s2)
+    (cond
+     ((and (list? s1) (list? s2)) (eqlist? s1 s2))
+     ((and (atom? s1) (atom? s2)) (eqan? s1 s2 ))
+     (else #f))))
+
+(define eqlist
+  (lambda (l1 l2)
+    (cond
+     ((and (null? l1) (null? l2)) #t)
+     ((or (null? l1) (null? l2)) #f)
+     (else
+      (and (equal? (car l1) (car l2))
+           (eqlist? (cdr l1) (cdr l2)))))))
+
+(define rember
+  (lambda (s l)
+    (cond
+      ((null? l) (quote ()))
+      ((equal? (car l) s) (cdr l))
+      (else (cons (car l)
+                  (rember s (cdr l)))))))
